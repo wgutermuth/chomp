b5fb4898dd2e85549c9f584bd405456f8e8ac1f6
wgutermuth <147719695+wgutermuth@users.noreply.github.com>
2023-10-12 09:56:03 -0400

    Initial game.

--- game.py
+++ game.py
@@ -1,2 +1,16 @@
-print('Hello world!')
-print('new message!')
+import pygame
+import time
+import random
+
+pygame.init()
+
+screen = pygame.display.set_mode((500,500))
+pygame.display.set_caption('Chomp!')
+
+pygame.draw.rect(screen,(100, 25, 0), (0, 380, 400, 400))
+
+while True:
+    time.sleep(1)
+    for event in pygame.event.get():
+        if event.type == 1024:
+            print('Cursor!')
