7e2ced7d6b87be4ba403a54738af879a2ba1c779
wgutermuth <147719695+wgutermuth@users.noreply.github.com>
2023-10-18 09:27:59 -0400

    Shift to variables instead of magic numbers.

--- game.py
+++ game.py
@@ -2,17 +2,26 @@ import pygame
 import time
 import sys
 
+# DIMENSIONS
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 400
+SAND_HEIGHT = 20
+TILE_SIZE = 64  # square, height = width
+
+# COLORS
+WATER_COLOR = (114, 159, 232)
+SAND_COLOR = (100, 25, 0)
+
 print(f" the quit event is type {pygame.QUIT}")
 pygame.init()
 
-
-screen = pygame.display.set_mode((400,400))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Chomp!")
-screen.fill((114,159,255))
-pygame.draw.rect(screen, (100,25,0), (0, 380, 400, 400))
+screen.fill(WATER_COLOR)
+pygame.draw.rect(screen, SAND_COLOR, (0, SCREEN_HEIGHT - SAND_HEIGHT, SCREEN_WIDTH, SAND_HEIGHT))
 sand = pygame.image.load("assets/images/sand.png").convert()
-screen.blit(sand, (200,200,64,64))
-pygame.draw.rect(screen, (0,255,0), (200,200, 50,50))
+screen.blit(sand, (200 - 32, 200 - 32))
+#pygame.draw.rect(screen, (0,255,0), (200,200, 50,50))
 pygame.display.flip()
 
 while True:
