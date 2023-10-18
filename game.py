c4759dacca5908d1435c22a2fc562e7d3b45d2c2
wgutermuth <147719695+wgutermuth@users.noreply.github.com>
2023-10-18 09:04:20 -0400

    Assets added.

--- game.py
+++ game.py
@@ -1,16 +1,26 @@
 import pygame
 import time
-import random
+import sys
 
+print(f" the quit event is type {pygame.QUIT}")
 pygame.init()
 
-screen = pygame.display.set_mode((500,500))
-pygame.display.set_caption('Chomp!')
 
-pygame.draw.rect(screen,(100, 25, 0), (0, 380, 400, 400))
+screen = pygame.display.set_mode((400,400))
+pygame.display.set_caption("Chomp!")
+screen.fill((114,159,255))
+pygame.draw.rect(screen, (100,25,0), (0, 380, 400, 400))
+sand = pygame.image.load("assets/images/sand.png").convert()
+screen.blit(sand, (200,200,64,64))
+pygame.draw.rect(screen, (0,255,0), (200,200, 50,50))
+pygame.display.flip()
 
 while True:
-    time.sleep(1)
     for event in pygame.event.get():
-        if event.type == 1024:
-            print('Cursor!')
+
+        if event.type == pygame.QUIT:
+            print("Thanks for playing")
+            pygame.quit()
+            sys.exit()
+
+        pygame.display.flip()
